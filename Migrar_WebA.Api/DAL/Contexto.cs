a92753eb6bbfd1e9d2754ff78680165da0c0ae13
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-12 21:16:00 -0400

    Creando el contexto

---
+++ Migrar_WebA.Api/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Migrar_WebA.Shared.Models;
+
+namespace Migrar_WebA.Api.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Prioridades> Prioridades { get; set; }
+    }
+}
