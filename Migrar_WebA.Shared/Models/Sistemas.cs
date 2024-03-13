47322ba22f19cbebd113ffad7548521c65aa0781
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-13 15:10:34 -0400

    Agregando contenido de los modelos

--- Migrar_WebA.Shared/Models/Sistemas.cs
+++ Migrar_WebA.Shared/Models/Sistemas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace Migrar_WebA.Shared.Models
 {
     public class Sistemas
     {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? NombreSistema { get; set; }
     }
 }
