4dc0ae0c1bb1bbe7af4b94ae984219d4dcbb7d1c
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-12 21:15:48 -0400

    Creando el Modelo de prioridades

---
+++ Migrar_WebA.Shared/Models/Prioridades.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrar_WebA.Shared.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "El descripcion es obligatorio")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Los dias de comprimiso deben ser validos")]
+        [Range(1, 31, ErrorMessage = "Los dias de comprimisos deben de estar entre 1 y 31")]
+        public int DiasCompromiso { get; set; }
+    }
+}
